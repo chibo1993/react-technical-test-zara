73351978fc2fe73d5426c5d13dc6fc4b8affea00
David Fernandez Garcia <dfernandezg@hiberus.com>
2023-04-01 16:13:43 +0000

    npm i

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -4,10 +4,21 @@ export default function DetailsPodcast() {
    console.log('entra', useFetchPodcastDetail());
    const detailsPodcast = useFetchPodcastDetail()
    console.log('detailsPodcast', detailsPodcast)
-return detailsPodcast.map(({country}) =>
+return detailsPodcast.map(({artistName, artworkUrl160, trackName, description, trackId}) =>
    <>
    {
-   <p>{country}</p>
+      <div className='content__details__podcast'>
+         <div className="content_podcasts_values">
+         <p>{artistName} <br />
+      by </p>
+      <img src={artworkUrl160} />
+      <p>{trackName}</p>
+      <p>{description}</p>
+      <p>{artistName}</p>
+      <p>{artistName}</p>
+         </div>
+      
+   </div>
    }
    </>
    )
--- src/services/getPodcastDetails.js
+++ src/services/getPodcastDetails.js
@@ -4,20 +4,24 @@ import time from "../helpers/time"
 export default async function getPodcastDetails() {
     console.log('--->');
     const podcastId = '1678378239'
-    const apiURL = `${BASE_URL}/lookup?id=${podcastId}&country=US&media=podcast&entity=podcastEpisode&limit=200`;
+    const apiURL = `${BASE_URL}/lookup?id=${podcastId}`;
     console.log('apiURL', apiURL)
 
     let data = [];
-    if (time() < 24 || window.localStorage.getItem('fetchPodcastDetail') === null) {
+    if (time() >= 24 || window.localStorage.getItem('fetchPodcastDetail') === null) {
         data = await fetch(apiURL)
         .then(res => res.json())
         .then(response => {
-            console.log('response', response);
         window.localStorage.setItem('fetchPodcastDetail', JSON.stringify(response.results))
         window.localStorage.setItem('LastFetchDate', new Date())
         console.log('response.results', response.results);
         return response.results
+        })
+        .catch(err => {
+         console.log(err)
         }) 
+    } else {
+        data = JSON.parse(window.localStorage.getItem('fetchPodcastDetail'))
     }
     return data;
 }
